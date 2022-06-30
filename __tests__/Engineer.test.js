f6204eea6811278a052a23969247858534166e38
Brain <ortiz_brian323@yahoo.com>
2022-06-30 15:29:13 -0700

    added intern tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,35 +1,35 @@
 const Engineer = require("../lib/Engineer");
 
 const engineerProperties = {
-  name: "Brian",
-  id: "1",
-  email: "brian@yahoo.com",
-  role: "Engineer",
-  github: "brian",
+    name: "Brian",
+    id: "1",
+    email: "brian@yahoo.com",
+    role: "Engineer",
+    github: "brian",
 };
 
 describe("Engineer", () => {
-  test("create engineer object test", () => {
-    const engineer = new Engineer(engineerProperties);
-    expect(typeof engineer).toBe("object");
-    expect(engineer.name).toBe("Brian");
-    expect(engineer.id).toBe("1");
-    expect(engineer.email).toBe("brian@yahoo.com");
-    expect(engineer.role).toBe("Engineer");
-    expect(engineer.github).toBe("brian");
-  });
-
-  describe("gitHub", () => {
-    test("testing github", () => {
-      const engineer = new Engineer(engineerProperties);
-      expect(engineer.github).toBe("brian");
+    test("create engineer object test", () => {
+        const engineer = new Engineer(engineerProperties);
+        expect(typeof engineer).toBe("object");
+        expect(engineer.name).toBe("Brian");
+        expect(engineer.id).toBe("1");
+        expect(engineer.email).toBe("brian@yahoo.com");
+        expect(engineer.role).toBe("Engineer");
+        expect(engineer.github).toBe("brian");
     });
-  });
 
-  describe("Engineer Role", () => {
-    test('testing getRole()', () => {
-      const engineer = new Engineer(engineerProperties);
-      expect(engineer.role).toBe("Engineer");
+    describe("gitHub", () => {
+        test("testing github", () => {
+            const engineer = new Engineer(engineerProperties);
+            expect(engineer.github).toBe("brian");
+        });
     });
-  });
+
+    describe("Engineer Role", () => {
+        test('testing getRole()', () => {
+            const engineer = new Engineer(engineerProperties);
+            expect(engineer.role).toBe("Engineer");
+        });
+    });
 });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,32 @@
 const Intern = require("../lib/Intern");
+const internProperties = {
+    name: "Brian",
+    id: "2",
+    email: "brian@yahoo.com",
+    role: "Intern",
+    school: "harvard",
+};
+describe("intern class", () => {
+    test("creating intern", () => {
+        const intern = new Intern(internProperties);
+        expect(typeof intern).toBe("object");
+        expect(intern.name).toBe("Brian");
+        expect(intern.id).toBe("2");
+        expect(intern.role).toBe("Intern");
+        expect(intern.school).toBe("harvard");
+    });
 
+    describe("intern role", () => {
+        test('testing getRole()', () => {
+            const intern = new Intern(internProperties);
+            expect(intern.role).toBe("Intern");
+        });
+    });
+
+    describe("intern school", () => {
+        test("testing getSchool()", () => {
+            const intern = new Intern(internProperties);
+            expect(intern.school).toBe("harvard");
+        });
+    });
+});
